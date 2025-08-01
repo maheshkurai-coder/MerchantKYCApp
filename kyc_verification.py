2886bbaad3cd05d6fbf04672d40068694679832e
Mahesh Rai <maheshkurai@gmail.com>
2025-08-01 14:44:36 +0530

    Changes

--- kyc_verification.py
+++ kyc_verification.py
@@ -5,6 +5,7 @@ st.title("Operations Team - KYC Verification")
 st.write("🔍 List of Submitted KYCs (Simulated)")
 st.write("1. Name: Ramesh Kumar | PAN: ABCDE1234F | Status: Pending")
 st.write("2. Name: Priya Sharma | PAN: XYZAB5678C | Status: Pending")
+print("hello")
 
 if st.button("Approve All"):
     st.success("All KYCs Approved and Synced with Government APIs (Simulated)")
